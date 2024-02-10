42405f106eeb53c146c7fd5c52fb8abe5b6f94da
SadikShaik11 <sadikshaik139@gmail.com>
2024-02-10 13:07:17 +0530

    ressolved error

--- src/modules/components/Home/HomeComponent.jsx
+++ src/modules/components/Home/HomeComponent.jsx
@@ -1,5 +1,5 @@
 import PopularMoviesComponent from "./PopularMoviesComponent";
-import TopRatedMoviesComponent from "./topRatedMoviesComponent";
+import TopRatedMoviesComponent from "./TopRatedMoviesComponent";
 import UpcomingMoviesComponent from "./UpcomingMoviesComponent";
 import Genre from "./Genre";
 import TrendingBanner from "./TrendingBanner";
